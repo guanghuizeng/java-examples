d1763ce5f9969a4ba18e3f79fc6c2ad5a189a0a8
Guanghui Zeng <guanghui.zeng@live.com>
2018-04-19 21:22:07 +0800

    测试FileAttributes

---
+++ src/main/java/corejava9/v2ch01/attributes/FileAtrributesTest.java
@@ -0,0 +1,28 @@
+package corejava9.v2ch01.attributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class FileAtrributesTest {
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("/Users/guanghuizeng/workspace/github.com/guanghuizeng/java-gems/xanadu.txt");
+
+        BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class, LinkOption.NOFOLLOW_LINKS);
+
+        System.out.printf("size: %d\n", attr.size());
+        System.out.printf("creationTime: %s\n", attr.creationTime());
+        System.out.println("isOther: " + attr.isOther());
+        System.out.printf("fileKey: %s\n", attr.fileKey());
+        System.out.printf("isRegularFile: %b\n", attr.isRegularFile());
+        System.out.println("isSymbolicLink: " + attr.isSymbolicLink());
+        System.out.printf("lastAccessTime: %s\n", attr.lastAccessTime().toString());
+        System.out.printf("lastModifiedTime: %s\n", attr.lastModifiedTime().toString());
+
+
+    }
+}
